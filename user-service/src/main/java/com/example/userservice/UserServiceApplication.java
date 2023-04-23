bf8075d35cdf14396eeb1d6700cfbef1a68329a0
김윤미 <yoonmihy@naver.com>
2023-04-23 19:55:56 +0900

    add : RestTemplate 사용

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient //클라이언트로 등록
@@ -16,5 +18,11 @@ public class UserServiceApplication {
 		@Bean
 		public BCryptPasswordEncoder passwordEncoder() {
 			return new BCryptPasswordEncoder(); //bean으로 등록해주기! 이래야 service에서 씀
+		}
+
+		@Bean
+		@LoadBalanced
+		public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 		}
 }
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,40 +4,43 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.jpa.UserEntity;
 import com.example.userservice.service.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository=userRepository;
-        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
-    }
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
+    private final RestTemplate restTemplate;
+
     @Override
     @Transactional
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString()); //랜덤 아이디 부여
-        ModelMapper mapper=new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        UserEntity user= mapper.map(userDto, UserEntity.class); //매핑!
-        String encryptedPwd=bCryptPasswordEncoder.encode(userDto.getPwd());
+        UserEntity user = mapper.map(userDto, UserEntity.class); //매핑!
+        String encryptedPwd = bCryptPasswordEncoder.encode(userDto.getPwd());
         user.setEncryptedPwd(encryptedPwd);
         userDto.setEncryptedPwd(encryptedPwd);
 
@@ -48,15 +51,22 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto getUserByUserId(String userId) {
-        UserEntity userEntity=userRepository.findByUserId(userId);
+        UserEntity userEntity = userRepository.findByUserId(userId);
 
-        if(userEntity==null) {
+        if (userEntity == null) {
             throw new UsernameNotFoundException("User not found");
         }
 
-        List<ResponseOrder> orders=new ArrayList<>();
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        UserDto userDto=new ModelMapper().map(userEntity, UserDto.class);
+        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+
+        /* Rest Template 사용 */
+        ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<ResponseOrder>>() {
+        });
+
+        List<ResponseOrder> orders = orderListResponse.getBody();
+
         userDto.setOrders(orders);
 
         return userDto;
@@ -69,17 +79,17 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto getUserDetailsByEmail(String email) {
-        UserEntity userEntity=userRepository.findByEmail(email);
-        if(userEntity==null) throw new UsernameNotFoundException(email);
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null) throw new UsernameNotFoundException(email);
         return new ModelMapper().map(userEntity, UserDto.class);
     }
 
     //이메일 가지고 사용자 찾아오는 메서드
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity=userRepository.findByEmail(username);
+        UserEntity userEntity = userRepository.findByEmail(username);
 
-        if(userEntity==null) throw new UsernameNotFoundException(username);
+        if (userEntity == null) throw new UsernameNotFoundException(username);
 
         //new ArrayList<>(): 로그인 되었을 때 할 수 있는 작업 중에서 권한을 추가하는 작업 넣으면 됨
         //지금은 권한 없어서 걍 빈 리스트
